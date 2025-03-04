1400268b7de38b2fee0a4fbdf22de1f6c39aa801
Nguyễn Phúc Bình <nguyenphucbinh67@gmail.com>
2025-03-04 13:13:59 +0700

    edit init

--- RStockvn/__init__.py
+++ RStockvn/__init__.py
@@ -9,9 +9,9 @@ __website__ = "https://github.com/NPhucBinh"
 
 
 
-from .update_package import (check_for_updates,updates_package_RStockvn)
+#from .update_package import (check_for_updates,updates_package_RStockvn)
 from . import user_agent
-from .chrome_driver.chromedriver_setup import (check_var, remove_file_old)
+
 from .stockvn import (get_foreign_historical_vnd,get_price_historical_vnd,key_id,list_company,update_company,report_finance_vnd,report_finance_cf,
     getCPI_vietstock,solieu_sanxuat_congnghiep,solieu_banle_vietstock,solieu_XNK_vietstock,solieu_FDI_vietstock,tygia_vietstock,solieu_tindung_vietstock,laisuat_vietstock,
     solieu_danso_vietstock,solieu_GDP_vietstock,get_data_result_order,get_info_cp,momentum_ck)
@@ -19,5 +19,3 @@ from .stockvn import (get_foreign_historical_vnd,get_price_historical_vnd,key_id
 from selenium import webdriver
 
 
-remove_file_old()
-check_var()
